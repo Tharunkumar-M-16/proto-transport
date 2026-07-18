a3c72a45ffa0ede331d1d8f353b3a6ccb7ad2bff
Tharun Kumar <m.tharunkumar9047@gmail.com>
2026-07-18 16:24:33 +0530

    Update API URLs to Render deployment and tsconfig updates

--- mobile-web/src/utils/constants.ts
+++ mobile-web/src/utils/constants.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = 'http://192.168.22.3:5000';
-export const SOCKET_URL = 'http://192.168.22.3:5000';
+export const API_BASE_URL = 'https://proto-transport.onrender.com';
+export const SOCKET_URL = 'https://proto-transport.onrender.com';
